fea94906a2a502443888ce9ef3112bb908d26ba1
pythonseo <896531509@qq.com>
2019-08-03 23:40:19 +0800

    mysql插入数据不显示中文问题

--- nezha.py
+++ nezha.py
@@ -1,13 +1,36 @@
 import requests
 import json
 from bs4 import BeautifulSoup
-url='https://movie.douban.com/subject/26794435/comments?start=0&limit=20&sort=new_score&status=P&comments_only=1'
-response=requests.get(url)
-con=json.loads(response.content)
-print(type(con))
-soup=BeautifulSoup(str(con),'lxml')
-print(soup.prettify())
-for author,comments in zip(soup.select('a[class='']'),soup.select('span[class="short"]')):
-    print(author.get_text(),comments.get_text())
-# for comments in soup.select('span[class="short"]'):
-#     print(comments.get_text())
+import pymysql
+class Nezhacoms(object):
+    def __init__(self,ip,user,password,db,charset='utf8'):
+        self.ip=ip
+        self.user=user
+        self.password=password
+        self.db=db
+        self.charset=charset
+        self.sqlcon=pymysql.connect(
+            host=self.ip,
+            user=self.user,
+            password=self.password,
+            db=self.db,
+            charset=self.charset)
+    def nezhaspyder(self,url):
+        response=requests.get(url)
+        con=json.loads(response.content)
+        soup=BeautifulSoup(str(con),'lxml')
+        for author,comments in zip(soup.select('a[class='']'),soup.select('span[class="short"]')):
+            self.nezhastore(str(comments.get_text()),str(author.get_text()))
+    def nezhastore(self,c,a):
+        cursor=self.sqlcon.cursor()
+        sql='insert into test0 set comments="{}",author="{}"'.format(c,a)
+        try:
+            cursor.execute(sql)
+            print('sucess')
+            self.sqlcon.commit()
+        except Exception as e:
+            print(e)
+if __name__=='__main__':
+    sqlzezha=Nezhacoms('localhost','root','123456','test',charset='utf8')
+    url = 'https://movie.douban.com/subject/26794435/comments?start=0&limit=20&sort=new_score&status=P&comments_only=1'
+    sqlzezha.nezhaspyder(url)
